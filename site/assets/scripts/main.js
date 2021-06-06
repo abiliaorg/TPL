8f3977dec2f34223cf6513d451245dc2c7349c18
Animesh Singh <animesh.singh.jay@gmail.com>
2021-06-07 04:58:15 +0530

    fixed some padding issues + *tried* owl carousel

--- site/assets/scripts/main.js
+++ site/assets/scripts/main.js
@@ -5,12 +5,12 @@ app.states = {};
 var loadNoCache = false;
 
 var statesToLoad = [
-	"01_load",
-	"02_home",
-	"03_avatar",
-	"04_interest",
-	"05_familytree",
-	"06_dexterslab",
+    "01_load",
+    "02_home",
+    "03_avatar",
+    "04_interest",
+    "05_familytree",
+    "06_dexterslab",
 ];
 
 var NstatesToLoad = statesToLoad.length; //+2 for the 2 scripts of settings
@@ -21,122 +21,122 @@ var NstylesLoaded = 0;
 
 
 $(document).ready(function() {
-    console.log( "Document ready!" );
-    
-    loadScript("./states/settings.js", onMainLoaded, onStateError);     
+    console.log("Document ready!");
+
+    loadScript("./states/settings.js", onMainLoaded, onStateError);
 });
 
 
-function LoadStates_Scripts(){
-    
+function LoadStates_Scripts() {
+
     for (s in statesToLoad) {
-		var c = loadNoCache ? "?" + new Date().getMilliseconds() : "";
-		loadScript("./states/"+statesToLoad[s]+".js" + c, onStateLoaded, onStateError);
-	}
+        var c = loadNoCache ? "?" + new Date().getMilliseconds() : "";
+        loadScript("./states/" + statesToLoad[s] + ".js" + c, onStateLoaded, onStateError);
+    }
 }
 
-function LoadStates_Styles(){
-    
+function LoadStates_Styles() {
+
     for (s in statesToLoad) {
-		var c = loadNoCache ? "?" + new Date().getMilliseconds() : "";
-		loadStyle("./assets/styles/"+statesToLoad[s]+".css" + c, onStyleLoaded);
-	}
+        var c = loadNoCache ? "?" + new Date().getMilliseconds() : "";
+        loadStyle("./assets/styles/" + statesToLoad[s] + ".css" + c, onStyleLoaded);
+    }
 }
 
 
 function loadScript(url, onsuccess, onerror) {
 
-	var script = document.createElement("script")
-	script.type = "text/javascript";
+    var script = document.createElement("script")
+    script.type = "text/javascript";
 
-	if (script.readyState) { //IE
-		script.onreadystatechange = function() {
-			if (script.readyState == "loaded" ||
-				script.readyState == "complete") {
-				script.onreadystatechange = null;
-				onsuccess(this.src);
-			}
-		};
-	} else { //Others
-		script.onload = function() {
-			onsuccess(this.src);
-		};
-	}
+    if (script.readyState) { //IE
+        script.onreadystatechange = function() {
+            if (script.readyState == "loaded" ||
+                script.readyState == "complete") {
+                script.onreadystatechange = null;
+                onsuccess(this.src);
+            }
+        };
+    } else { //Others
+        script.onload = function() {
+            onsuccess(this.src);
+        };
+    }
 
-	script.onerror = function() {
-		onerror();
-	};
+    script.onerror = function() {
+        onerror();
+    };
 
-	script.src = url;
-	document.getElementsByTagName("head")[0].appendChild(script);
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 function loadStyle(url, callback) {
 
-	var link = document.createElement("link")
-	link.rel = "stylesheet";
+    var link = document.createElement("link")
+    link.rel = "stylesheet";
 
-	if (link.readyState) { //IE
-		link.onreadystatechange = function() {
-			if (link.readyState == "loaded" ||
-				link.readyState == "complete") {
-				link.onreadystatechange = null;
-				callback();
-			}
-		};
-	} else { //Others
-		link.onload = function() {
-			callback();
-		};
-	}
+    if (link.readyState) { //IE
+        link.onreadystatechange = function() {
+            if (link.readyState == "loaded" ||
+                link.readyState == "complete") {
+                link.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else { //Others
+        link.onload = function() {
+            callback();
+        };
+    }
 
-	link.href = url;
-	document.getElementsByTagName("head")[0].appendChild(link);
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
 }
 
-function onMainLoaded(){
+function onMainLoaded() {
     LoadStates_Scripts();
     LoadStates_Styles();
 }
 
 function onStateLoaded() {
 
-	NstatesLoaded++;
-	console.log("🚀 State " + NstatesLoaded + " of " + NstatesToLoad + " loaded");
-	//updateLoadingBar();
+    NstatesLoaded++;
+    console.log("🚀 State " + NstatesLoaded + " of " + NstatesToLoad + " loaded");
+    //updateLoadingBar();
 
-	if (NstatesLoaded == NstatesToLoad) {
-		Ready();
-	}
+    if (NstatesLoaded == NstatesToLoad) {
+        Ready();
+    }
 }
 
 function onStateError() {
-	console.log("🚩 Error while loading state");
+    console.log("🚩 Error while loading state");
 }
 
 function Ready() {
-	console.log("🏁 Everything is loaded");
-	
-    window.setTimeout(function(){
-	    goToState("load");
+    console.log("🏁 Everything is loaded");
+
+    window.setTimeout(function() {
+        goToState("load");
     }, 1000);
-    
+
 }
 
 function onStyleLoaded() {
     NstylesLoaded++;
-	console.log("😎 Style " + NstylesLoaded + " of " + NstylesToLoad + " loaded");
+    console.log("😎 Style " + NstylesLoaded + " of " + NstylesToLoad + " loaded");
 }
 
 
-function updateView(){
+function updateView() {
     window.app.view.state.hide();
-    $("#"+window.app.settings.currentState).fadeIn("slow");
-    window.app.view.info.text("State: "+window.app.settings.currentState);
+    $("#" + window.app.settings.currentState).fadeIn("slow");
+    window.app.view.info.text("State: " + window.app.settings.currentState);
     document.title = window.app.settings.currentState;
 }
 
-function goToState(state){
+function goToState(state) {
     window.app.settings.currentState = state;
     updateView();
     window.app.states[state].Run();
--- site/states/03_avatar.js
+++ site/states/03_avatar.js
@@ -1,26 +1,47 @@
 function State_Avatar() {
 
-	var _this = {};
+    var _this = {};
 
-	_this.Iam = function() {
-		return "avatar";
-	};
+    _this.Iam = function() {
+        return "avatar";
+    };
 
-	_this.Run = function() {
-		console.log("⛳ ------> Avatar state");
-		
-		
-		
+    _this.Run = function() {
+        console.log("⛳ ------> Avatar state");
 
-	};
+        $(".owl-carousel").owlCarousel();
+
+        $(function() {
+
+            $('.loop').on('initialized.owl.carousel translate.owl.carousel', function(e) {
+                idx = e.item.index;
+                $('.owl-item.big').removeClass('big');
+                $('.owl-item.medium').removeClass('medium');
+                $('.owl-item').eq(idx).addClass('big');
+                $('.owl-item').eq(idx - 1).addClass('medium');
+                $('.owl-item').eq(idx + 1).addClass('medium');
+            });
 
 
-	window.app.view.button_avatar_done.on("click", function(){
+            $('.loop').owlCarousel({
+                center: true,
+                items: 5,
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                autoPlay: 3000
+            })
+        });
+
+
+    };
+
+
+    window.app.view.button_avatar_done.on("click", function() {
         goToState("interest");
     });
 
-	return _this;
-
+    return _this;
 
 }
 
